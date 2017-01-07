8267c27e8fafa9f9760b80c4dc0ca96d612521b7
Denisa Iacob <denisaiacob18@gmail.com>
2017-01-07 12:13:17 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FortuneTellerProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Console.WriteLine("Hello there, welcome to Fortune Teller!");
+            Console.WriteLine("What is your first name?");
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("What is your last name?");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine("Please enter your age.");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter your birth month (digits only).");
+            int birthMonth = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("What is your favorite ROYGBIV color?" + "\n" + "Please enter \"help\" for description of ROYGBIV.");
+            string response = Console.ReadLine();
+
+            if (response == "red")
+            {
+                Console.WriteLine("Thank you!");
+            }
+
+            else if (response == "orange")
+            {
+                Console.WriteLine("Thank you!");
+            }
+
+            else if (response == "yellow")
+            {
+                Console.WriteLine("Thank you!");
+            }
+
+            else if (response == "green")
+            {
+                Console.WriteLine("Thank you!");
+            }
+
+            else if (response == "blue")
+            {
+                Console.WriteLine("Thank you!");
+            }
+
+            else if (response == "indigo")
+            {
+                Console.WriteLine("Thank you!");
+            }
+
+            else if (response == "violet")
+            {
+                Console.WriteLine("Thank you!");
+            }
+
+            else if (response == "help")
+            {
+                Console.WriteLine("Please seclect from the folowing colors: red, orange, yellow, green, blue, indigo or violet.");
+                 string responseHelp = Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Sorry that is not an option, try again.");
+            }
+            
+                Console.WriteLine("Enter the number of sibilings that you have.");
+                int siblings = int.Parse(Console.ReadLine());
+           */
+
+
+        }
+    }
+}
