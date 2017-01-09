d39d91b4132c559acfba81e8bac5c767f397b7e9
Denisa Iacob <denisaiacob18@gmail.com>
2017-01-08 21:44:23 -0500

    final changes

--- Program.cs
+++ Program.cs
@@ -12,28 +12,26 @@ namespace FortuneTellerProject
         {
           
             Console.WriteLine("Hello there, welcome to Fortune Teller!");
+            //start asking user for input
 
             Console.WriteLine("To move forward with your fortune, enter your first name.");
-            string firstName = Console.ReadLine();
-            string firstNameL = firstName.ToLower();
+            string firstName = Console.ReadLine();       
 
             Console.WriteLine("Thank you " + firstName + ". Next enter your last name.");
             string lastName = Console.ReadLine();
-            string lastNameL = lastName.ToLower();
 
             Console.WriteLine(firstName +" " + lastName + ", please enter your age.");
             int age = int.Parse(Console.ReadLine());
+            string oddOReven = "";
 
-            //if (age % 2 == 0)
-            //{
-            //   Console.WriteLine("You will retire in 22 years.");
-            //}
-            //else
-            //{
-            //    Console.WriteLine("You will retire in 35 years.");
-            //}
-
-
+            if (age % 2 == 0)
+            {
+                oddOReven = ("You will retire in 22 years");
+            }
+            else
+            {
+                oddOReven = ("You will retire in 35 years");
+            }
 
             Console.WriteLine("Now that I know your age, please enter your birth month (digits only).");
             int birthMonth = int.Parse(Console.ReadLine());
@@ -41,15 +39,15 @@ namespace FortuneTellerProject
 
             if (birthMonth >= 1 && birthMonth <= 4)
             {
-                money = 300f;
+                money = 3.45f;
             }
             else if (birthMonth >= 5 && birthMonth <= 8)
             {
-                money = 4.0f;
+                money = 4.6f;
             }
             else if (birthMonth >= 9 && birthMonth <= 12)
             {
-                money = 500f;
+                money = 500.56f;
             }
             else
             {
@@ -58,70 +56,76 @@ namespace FortuneTellerProject
 
             Console.WriteLine("To contiue with your fortune reader, what is your favorite ROYGBIV color?" + "\n" + "Please enter \"help\" for description of ROYGBIV.");
             string response = Console.ReadLine();
-            
+            string responseCaps = response.ToUpper();
 
-            if (response == "help")
+            string userTransport = "";
+
+            if (responseCaps == "HELP")
             {
                 Console.WriteLine("ROYGBIV COLORS: \nR is for Red. \nO is for Orange. \nY is for Yellow. \nG is for Green. \nB is for Blue. \nI is for Indigo. \nV is for Violet. \nEnter a ROYGBIV color: ");
                 response = Console.ReadLine();
             }
 
-            //switch (response)
-            //{
-            //    case "red":
-            //        Console.WriteLine("You will retire with a boat");
-            //        break;
-            //    case "orange":
-            //        Console.WriteLine("You will retire with a Ford SUV");
-            //        break;
-            //    case "yellow":
-            //        Console.WriteLine("You will retire with a motorcycle");
-            //        break;
-            //    case "green":
-            //        Console.WriteLine("You will retire with a Rolls Royce");
-            //        break;
-            //    case "blue":
-            //        Console.WriteLine("You will retire with a bike");
-            //        break;
-            //    case "indigo":
-            //        Console.WriteLine("You will retire with a yacht");
-            //        break;
-            //    case "violet":
-            //        Console.WriteLine("You will retire with an airplane");
-            //        break;
-            //}
-         
+            switch (responseCaps)
+            {
+                case "RED":
+                    userTransport = ("you will own a boat");
+                    break;
+                case "ORANGE":
+                    userTransport = ("you will own a Ford SUV");
+                    break;
+                case "YELLOW":
+                   userTransport = ("you will own a motorcycle");
+                    break;
+                case "GREEN":
+                    userTransport = ("you will own a Rolls Royce");
+                    break;
+                case "BLUE":
+                    userTransport = ("you will own a bike");
+                    break;
+                case "INDIGO":
+                    userTransport = ("you will own a yacht");
+                    break;
+                case "VIOLET":
+                    userTransport = ("you will own an airplane");
+                    break;
+            }
+
 
             Console.WriteLine("Enter the number of sibilings that you have.");
                 int siblings = int.Parse(Console.ReadLine());
+            string userVaca = "";
 
+            if (siblings == 0)
+            {
+                userVaca =("You will have a beach house in Cancun");
+            }
 
-            //if (siblings == 0)
-            //{
-            //   Console.WriteLine("You will have a beach house in Cancun");
-            //}
-            
-            //else if (siblings == 1)
-            //{
-            //    Console.WriteLine("You will have a vacation home in Colorado");
-            //}
-            //else if (siblings == 2)
-            //{
-            //    Console.WriteLine("You will have a vacation home in Spain");
-            //}
-            //else if (siblings == 3)
-            //{
-            //   Console.WriteLine("You will have a beach house in Croatia");
-            //}
-            //else if (siblings > 3)
-            //{
-            //    Console.WriteLine("You will have a beach house in Australia");
-            //}
-            //else
-            //{
-            //    Console.WriteLine("You will be stuck in Crappy Ohio");
-            //}
-            
+            else if (siblings == 1)
+            {
+                userVaca = ("You will have a vacation home in Colorado");
+            }
+            else if (siblings == 2)
+            {
+                userVaca = ("You will have a vacation home in Spain");
+            }
+            else if (siblings == 3)
+            {
+                userVaca= ("You will have a beach house in Croatia");
+            }
+            else if (siblings > 3)
+            {
+                userVaca = ("You will have a beach house in Australia");
+            }
+            else
+            {
+                userVaca = ("You will be stuck in Crappy Ohio");
+            }
+
+            // Writing out the fortune 
+            Console.WriteLine(firstName +" " + lastName + " your fortune is... " + oddOReven + " with $" + money + " million in the bank. " + (userVaca) + " and " + userTransport + ".");
+            Console.WriteLine("Pretty awesome life ahead of you! Thanks for playing :)");
+            Console.ReadKey();
         }
     }
 }
